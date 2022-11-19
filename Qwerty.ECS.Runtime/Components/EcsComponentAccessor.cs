ae72520b7b3f89fec25e1cfbe7fa950df8f52420
voledyhil <voledyhil@gmail.com>
2022-11-19 17:27:27 +0300

    revert EcsComponentAccessor

--- Qwerty.ECS.Runtime/Components/EcsComponentAccessor.cs
+++ Qwerty.ECS.Runtime/Components/EcsComponentAccessor.cs
@@ -3,20 +3,44 @@ using Qwerty.Unsafe.Runtime;
 // ReSharper disable once CheckNamespace
 namespace Qwerty.ECS.Runtime.Components
 {
-	public readonly struct EcsComponentAccessor<T> where T : struct, IEcsComponent
+	public struct EcsComponentAccessor<T> where T : struct, IEcsComponent
 	{
 		private readonly unsafe UnsafeArray* m_buckets;
+		private UnsafeArray m_lastBucket;
+		private int m_lastBucketIndex;
+
 		internal unsafe EcsComponentAccessor(UnsafeArray* buckets)
 		{
 			m_buckets = buckets;
+			m_lastBucket = default;
+			m_lastBucketIndex = -1;
 		}
 
-		public unsafe T this[in EcsEntity entity]
+		public T this[in EcsEntity entity]
 		{
-			get => m_buckets->Get<UnsafeArray>(entity.Index / EcsComponentPool<T>.BucketSize)
-				.Read<T>(entity.Index % EcsComponentPool<T>.BucketSize);
-			set => m_buckets->Get<UnsafeArray>(entity.Index / EcsComponentPool<T>.BucketSize)
-				.Write(entity.Index % EcsComponentPool<T>.BucketSize, value);
+			
+			get
+			{
+				FindBucket(entity.Index);
+				return m_lastBucket.Read<T>(entity.Index % EcsComponentPool<T>.BucketSize);
+			}
+			set
+			{
+				FindBucket(entity.Index);
+				m_lastBucket.Write(entity.Index % EcsComponentPool<T>.BucketSize, value);
+			}
+		}
+		
+		private unsafe void FindBucket(int entityIndex)
+		{
+			int bucketIndex = entityIndex / EcsComponentPool<T>.BucketSize;
+			if (bucketIndex == m_lastBucketIndex)
+			{
+				return;
+			}
+
+			m_lastBucket = m_buckets->Get<UnsafeArray>(bucketIndex);
+			m_lastBucketIndex = bucketIndex;
 		}
 	}
 }
