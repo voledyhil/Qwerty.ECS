85ec82dcd2b3e8f145b8ea9a38fd6a53caf83de7
voledyhil <voledyhil@gmail.com>
2023-03-25 15:43:11 +0300

    memory unitl to public

--- Qwerty.ECS.Runtime/MemoryUtil.cs
+++ Qwerty.ECS.Runtime/MemoryUtil.cs
@@ -5,36 +5,36 @@ using System.Runtime.InteropServices;
 // ReSharper disable once CheckNamespace
 namespace Qwerty.ECS.Runtime
 {
-	internal static class MemoryUtil
+	public static class MemoryUtil
 	{
-		internal static IntPtr Alloc<T>() where T : struct
+		public static IntPtr Alloc<T>() where T : struct
 		{
 			return Alloc((uint)SizeOf<T>());
 		}
 		
-		internal static unsafe IntPtr Alloc(uint sizeInBytes)
+		public static unsafe IntPtr Alloc(uint sizeInBytes)
 		{
 			IntPtr ptr = Marshal.AllocHGlobal((int)sizeInBytes);
 			Unsafe.InitBlock((void*)ptr, 0, sizeInBytes);
 			return ptr;
 		}
 		
-		internal static void Free(IntPtr ptr)
+		public static void Free(IntPtr ptr)
 		{
 			Marshal.FreeHGlobal(ptr);
 		}
 
-		internal static int SizeOf<T>() where T : struct
+		public static int SizeOf<T>() where T : struct
 		{
 			return Unsafe.SizeOf<T>();
 		}
 
-		internal static unsafe T Read<T>(IntPtr ptr, int offset) where T : struct
+		public static unsafe T Read<T>(IntPtr ptr, int offset) where T : struct
 		{
 			return Unsafe.Read<T>((void*)(ptr + offset));
 		}
 		
-		internal static unsafe void Write<T>(IntPtr ptr, int offset, T value) where T : struct
+		public static unsafe void Write<T>(IntPtr ptr, int offset, T value) where T : struct
 		{
 			Unsafe.Write((void*)(ptr + offset), value);
 		}
