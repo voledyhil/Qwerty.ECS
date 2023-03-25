1ede8c2880180645ca110ca888e4c459fcabbbf0
voledyhil <voledyhil@gmail.com>
2023-03-25 15:02:53 +0300

    add multithread

--- Qwerty.ECS.Runtime/Jobs/JobExtensions.cs
+++ Qwerty.ECS.Runtime/Jobs/JobExtensions.cs
@@ -18,12 +18,15 @@ namespace Qwerty.ECS.Runtime.Jobs
         public static void Run<T>(this T jobData, EcsArchetypeGroup archetypeGroup, int innerLoopBatchCount)
             where T : struct, IParallelForJobChunk
         {
-#if UNITY_EDITOR
             int chunksCount = archetypeGroup.CalculateChunksCount();
             IntPtr chunks = GetChunks(archetypeGroup.archetypesChunks, archetypeGroup.archetypesCount, chunksCount);
+#if UNITY_EDITOR
             Schedule(jobData, chunksCount, innerLoopBatchCount, chunks).Complete();
-            MemoryUtil.Free(chunks);
+#else
+            ParallelForWorker worker = new ParallelForWorker();
+            worker.Execute(jobData, chunksCount, innerLoopBatchCount, chunks);
 #endif
+            MemoryUtil.Free(chunks);
         }
 
         private static unsafe IntPtr GetChunks(IntPtr archetypes, int archetypesCount, int chunksCount)
@@ -55,6 +58,84 @@ namespace Qwerty.ECS.Runtime.Jobs
 #endif
             public IntPtr chunks;
         }
+
+#if UNITY_EDITOR
+        private static unsafe JobHandle Schedule<T>(this T jobData, int arrayLength, int innerLoopBatchCount,
+            IntPtr chunks) where T : struct, IParallelForJobChunk
+        {
+            JobChunkWrapper<T> wrapper = new JobChunkWrapper<T>()
+            {
+                jobData = jobData,
+                chunks = chunks
+            };
+
+            JobsUtility.JobScheduleParameters scheduleParams = new JobsUtility.JobScheduleParameters(
+                UnsafeUtility.AddressOf(ref wrapper), ParallelForJobChunk<T>.Initialize(), default,
+                ScheduleMode.Parallel);
+
+            return JobsUtility.ScheduleParallelFor(ref scheduleParams, arrayLength, innerLoopBatchCount);
+        }
+#else
+
+        private class ParallelForWorker
+        {
+            private struct JobState<T> where T : struct, IParallelForJobChunk
+            {
+                public JobChunkWrapper<T> wrapper;
+                public int from;
+                public int to;
+            }
+            
+            private int m_threads;
+            public void Execute<T>(T jobData, int arrayLength, int innerLoopBatchCount, IntPtr chunks) where T : struct, IParallelForJobChunk
+            {
+                JobChunkWrapper<T> wrapper = new JobChunkWrapper<T>()
+                {
+                    jobData = jobData,
+                    chunks = chunks
+                };
+
+                m_threads = Math.Max(arrayLength / innerLoopBatchCount, 1);
+
+                int threads = m_threads;
+                int remainder = arrayLength % threads;
+                int slice = arrayLength / threads + (remainder == 0 ? 0 : 1);
+                for (int t = 0; t < threads; t++)
+                {
+                    int from = t * slice;
+                    int to = from + slice;
+                    if (to > arrayLength)
+                        to = arrayLength;
+
+                    if (from != to)
+                    {
+                        ThreadPool.QueueUserWorkItem(queueOnThread, new JobState<T> { wrapper = wrapper, from = from, to = to, }, true);
+                    }
+                    else
+                    {
+                        Interlocked.Decrement(ref m_threads);
+                    }
+                }
+                while (m_threads > 0) { }
+            }
+
+            private unsafe void queueOnThread<T>(JobState<T> state) where T : struct, IParallelForJobChunk
+            {
+                JobChunkWrapper<T> wrapper = state.wrapper;
+                IntPtr chunks = wrapper.chunks;
+                T jobData = wrapper.jobData;
+
+                for (int index = state.from; index < state.to; index++)
+                {
+                    EcsChunk* chunk = (EcsChunk*)MemoryUtil.Read<IntPtr>(chunks, index * sizeof(IntPtr));
+                    jobData.Execute(new EcsChunkAccessor(chunk));
+                }
+
+                Interlocked.Decrement(ref m_threads);
+            }
+        }
+#endif
+
 
 #if UNITY_EDITOR
         internal struct ParallelForJobChunk<T> where T : struct, IParallelForJobChunk
@@ -97,22 +178,6 @@ namespace Qwerty.ECS.Runtime.Jobs
                     }
                 }
             }
-        }
-
-        private static unsafe JobHandle Schedule<T>(this T jobData, int arrayLength, int innerLoopBatchCount,
-            IntPtr chunks) where T : struct, IParallelForJobChunk
-        {
-            JobChunkWrapper<T> wrapper = new JobChunkWrapper<T>()
-            {
-                jobData = jobData,
-                chunks = chunks
-            };
-
-            JobsUtility.JobScheduleParameters scheduleParams = new JobsUtility.JobScheduleParameters(
-                UnsafeUtility.AddressOf(ref wrapper), ParallelForJobChunk<T>.Initialize(), default,
-                ScheduleMode.Parallel);
-
-            return JobsUtility.ScheduleParallelFor(ref scheduleParams, arrayLength, innerLoopBatchCount);
         }
 #endif
     }
